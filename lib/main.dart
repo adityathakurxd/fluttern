b8ae0f9466f3c19d669f1a0619b61ca9a17ff3c5
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-11-03 22:03:57 +0530

    Hosted web app on Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttern/constants/constants.dart';
 import 'package:fluttern/screens/home_screen.dart';
 
 void main() {
-  runApp(ProviderScope(child: const MyApp()));
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Team Aditya',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.from(
-        colorScheme: ColorScheme.light(
-          primary: kPrimaryColor,
-          secondary: kAccentColor,
-        ),
-      ),
-      home: const HomeScreen(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Team Aditya',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData.from(
+              colorScheme: ColorScheme.light(
+                primary: kPrimaryColor,
+                secondary: kAccentColor,
+              ),
+            ),
+            home: const HomeScreen(),
+          );
+        }
+        return CircularProgressIndicator();
+      },
     );
+
   }
 }
